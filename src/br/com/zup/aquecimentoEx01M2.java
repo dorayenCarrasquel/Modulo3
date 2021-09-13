f47881f0210bf2bcd365e7d11e61bcc83b866f3c
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-13 12:08:30 -0300

    primeiro commit

---
+++ src/br/com/zup/aquecimentoEx01M2.java
@@ -0,0 +1,66 @@
+package br.com.zup;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class aquecimentoEx01M2 {
+    //1. Crie um programa para gerenciar uma lista de produtos
+// de um estabelecimento, cada produto tem um nome e um preço.
+// No final do programa, o usuário deve decidir se irá adicionar
+// mais de um produto, exibir todos os produtos cadastrados ou
+// excluir um produto pelo nome.
+    public static void main(String[] args) {
+        //Scanner, hashMap
+        Scanner leitor = new Scanner(System.in);
+        Map<String, Double> productos = new HashMap<String, Double>();
+        //variaveis
+
+        int menuPrincipal = 0;
+
+        while (menuPrincipal != 4) {
+            //menu
+            System.out.println("***** Mercado Real *****");
+            System.out.println("** Cadastro de Productos **");
+            System.out.println("[1] Cadastrar");
+            System.out.println("[2] Exivir");
+            System.out.println("[3] Excluir");
+            System.out.println("[4] Salir");
+
+            System.out.println("Marque uma opcão: ");
+            menuPrincipal = leitor.nextInt();
+            leitor.nextLine();
+            switch (menuPrincipal) {
+                case 1:
+                    //Entrada de dados
+                    System.out.println("[1] Cadastro de produtos");
+//                    System.out.println("Quantos produtos desea cadastrar");
+//                    int cadastro = leitor.nextInt();
+//                    leitor.nextLine();
+                    System.out.println("Indique o nome do produto");
+                    String nomeProduto = leitor.nextLine();
+                    System.out.println("Indique o valor do produto");
+                    Double precoProducto = leitor.nextDouble();
+
+                    //Cadastro
+                        productos.put(nomeProduto, precoProducto);
+                        System.out.println("cadastrado com suceso");
+                break;
+                case 2:
+                    for (String referencia: productos.keySet()) {
+                        System.out.println(referencia + "\t" +productos.get(referencia));
+                    }
+                break;
+                case 4:
+                    System.out.println("Vuelva Siempre");
+                menuPrincipal = 4;
+
+                default:
+                    System.out.println("Digite uma opção valida");
+            }
+
+        }
+
+
+    }
+}
